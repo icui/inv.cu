e3913bb5013cd41adc8cde98d8c9d94f4e725789
ccui <ccui@princeton.edu>
2019-05-22 14:33:10 -0400

    c

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nvcc src/inv.cu --std=c++11 -arch=sm_50 -lcublas -lcusolver -lcufft -o inv.out
 if [[ $1 ]]; then
-    nvcc utils/generate_checker.cu --std=c++11 -arch=sm_50 -lcublas -lcusolver -lcufft -o checker.out
+    nvcc src/checker.cu --std=c++11 -arch=sm_50 -lcublas -lcusolver -lcufft -o checker.out
 fi
